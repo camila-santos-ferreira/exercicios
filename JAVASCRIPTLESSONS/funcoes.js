335340f85ec60436eac5972c1e7c301b00a2af7f
Camila Ferreira <85899405+camila-santos-ferreira@users.noreply.github.com>
2021-08-20 23:35:53 -0300

    Exercícios

---
+++ JAVASCRIPTLESSONS/funcoes.js
@@ -0,0 +1,99 @@
+// function atravessar() {
+//     console.log("Olhar para a direita")
+//     console.log("Olhar para a esquerda")
+//     console.log("Atravessar")
+// }
+// atravessar()
+
+
+// function imprimir(i) {
+//     while(i < 100){
+//     console.log(i)
+//     i++
+//     }
+// }
+// imprimir(1)
+
+
+// function imprimir(x) {
+//      console.log(x)
+// }
+// // function multiplicar(a,b) {
+// //     imprimir(a*b)
+// // }
+// // multiplicar(3,5)
+
+
+// function multiplicar(a,b) {
+//     return a*b
+// }
+// let resultado = multiplicar(3,5)
+// imprimir(resultado)
+
+
+
+
+// function imprimir(nome) {
+//     console.log(nome)
+// }
+// imprimir("Camila")
+
+
+
+
+
+// Exercício 1
+// function mesmoNome(primeiroNome, segundoNome) {
+//     return primeiroNome == segundoNome
+// }
+
+// let comparacao = mesmoNome("A", "A")
+// console.log(comparacao)
+
+
+
+
+// Exercício 2
+// function maiorDeIdade(idade) {
+//     return idade >= 18
+// }
+// let idade = maiorDeIdade(13)
+// console.log(idade)
+
+
+
+// Exercício 3
+// function acrescerJuros(valorBoleto) {
+//     return valorBoleto * 1.10
+// }
+// let valorComJuros = acrescerJuros(1000)
+// console.log(valorComJuros)
+
+
+// Exercício 4 -------------------------------- Arrumar
+// function calculoMedia(itens) {
+    
+//     somatorio = 0
+
+//     for (i = 0; i < itens.length; i) {
+//         somatorio += itens[i]
+//     }
+    
+//     return somatorio / itens.length
+// }
+
+// var media = calculoMedia([5,10,5,10,5])
+// console.log(media)
+
+
+
+
+//Exercício 05
+// function percentualMargem(rlv, cpv) {
+//     lb = rlv - cpv
+//     return (lb/rlv)*100
+// }
+
+// let percentual = percentualMargem(100,10)
+// console.log(percentual)
+
